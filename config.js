8008c3407fc2ca60d1392027417849b5aef34b33
FabiJGM <108239086+FabiJGM@users.noreply.github.com>
2022-06-26 16:56:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰
 global.owner = [
-  ['573005929265', 'GataBot-MD 💖🐈', true],
+  ['56973163359, 'GataBot-MD 💖🐈', true],
   ['573212820425'],
   ['593987101976'],
   ['393924171285'],
